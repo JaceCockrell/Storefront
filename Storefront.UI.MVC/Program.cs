4789a906b7e17fa4588ee7e23d40c82d8cd35826
Jace Cockrell <jace.c.IT@gmail.com>
2023-06-12 16:24:20 -0500

    created Identity samples

--- Storefront.UI.MVC/Program.cs
+++ Storefront.UI.MVC/Program.cs
@@ -10,7 +10,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddRoleManager<RoleManager<IdentityRole>>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
